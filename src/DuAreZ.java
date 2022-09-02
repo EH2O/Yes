20421e14115a56471139544306306df7d22a6098
Edwimon <edwin.o.h@hotmail.se>
2022-09-02 13:33:07 +0200

    All is done

--- src/DuAreZ.java
+++ src/DuAreZ.java
@@ -3,40 +3,42 @@ import javax.swing.*;
 public class DuAreZ {
     public static void main(String[] args) {
         String ToEnOrDe = JOptionPane.showInputDialog(null, "Enter the message to encrypt or decrypt");
-        String Key = JOptionPane.showInputDialog(null, "Enter the fucking key");
+        String Key = JOptionPane.showInputDialog(null, "Enter the key");
         String CrypOrDe = "";
         String test ="";
         String Temp = "";
+        String Temps = "";
         int x = 0;
         if(ToEnOrDe.charAt(0) == '0' && ToEnOrDe.charAt(1) == 'X')
         {
             ToEnOrDe = ToEnOrDe.substring(2, ToEnOrDe.length());
-            System.out.println(ToEnOrDe);
 
-            for(int y = 0; y < ToEnOrDe.length()-1; y++) {
+            for(int y = 0; y < ToEnOrDe.length(); y += 2) {
                 Temp = ""+ToEnOrDe.charAt(y)+""+ToEnOrDe.charAt(y+1);
                 char character = (char) Integer.parseInt(Temp, 16);
                 test += character;
-                y++;
-                System.out.println(test);
             }
             for (int i = 0; i < test.length(); i++) {
-                if (x > Key.length()) {
+                if (x >= Key.length()) {
                     x = 0;
                 }
                 CrypOrDe += (char)maow(test.charAt(i), Key.charAt(x));
-                System.out.println(test +  "  " + CrypOrDe);
+                x++;
             }
 
         }
        else{
-            System.out.println("T2est");
+
             for (int i = 0; i < ToEnOrDe.length(); i++) {
-                if(x > Key.length()){
+                if(x >= Key.length()){
                     x = 0;
                 }
-                CrypOrDe += Integer.toHexString(maow(ToEnOrDe.charAt(i), Key.charAt(x)));
-
+                Temps = Integer.toHexString(maow(ToEnOrDe.charAt(i), Key.charAt(x)));
+                if(Temps.length() != 2){
+                    Temps = '0' + Temps;
+                }
+                CrypOrDe += Temps;
+                x++;
             }
         }
 
